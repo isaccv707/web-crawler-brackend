f82b91a574160a3b36393ac329ceee32d24d6822
isaac <isaac.cardenas1238@alumnos.udg.mx>
2024-05-07 23:41:26 -0600

    i

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,80 @@
 // backend/server.ts
-import express from 'express';
-import puppeteer from 'puppeteer';
-import fs from 'fs/promises';
-import cors from 'cors';
-import { envs } from "./config/env";
+import express from 'express'
+import puppeteer from 'puppeteer'
+import fs from 'fs/promises'
+import cors from 'cors'
+import { envs } from './config/env'
+import chromium from '@sparticuz/chromium'
 
-const app = express();
-const PORT = envs.PORT ;
+const app = express()
+const PORT = envs.PORT
 
 interface PageInfo {
-    title: string;
-    description: string;
-    keywords: string[];
-    links: string[];
-    images: string[];
-    text: string;
+  title: string
+  description: string
+  keywords: string[]
+  links: string[]
+  images: string[]
+  text: string
 }
 
-app.use(cors());
+app.use(cors())
 
-async function getPageInfo(url: string): Promise<PageInfo> {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+async function getPageInfo (url: string): Promise<PageInfo> {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true
+  })
+  const page = await browser.newPage()
+  await page.goto(url)
 
-    const pageInfo: PageInfo = await page.evaluate(() => {
-        const title = document.title;
-        const description = (document.querySelector('meta[name="description"]') as HTMLMetaElement)?.content || '';
-        const keywords = ((document.querySelector('meta[name="keywords"]') as HTMLMetaElement)?.content || '').split(',');
-        const links = Array.from(document.querySelectorAll('a')).map(link => link.href);
-        const images = Array.from(document.querySelectorAll('img')).map(img => img.src);
-        const text = document.body.innerText;
+  const pageInfo: PageInfo = await page.evaluate(() => {
+    const title = document.title
+    const description =
+      (document.querySelector('meta[name="description"]') as HTMLMetaElement)
+        ?.content || ''
+    const keywords = (
+      (document.querySelector('meta[name="keywords"]') as HTMLMetaElement)
+        ?.content || ''
+    ).split(',')
+    const links = Array.from(document.querySelectorAll('a')).map(
+      link => link.href
+    )
+    const images = Array.from(document.querySelectorAll('img')).map(
+      img => img.src
+    )
+    const text = document.body.innerText
 
-        return { title, description, keywords, links, images, text };
-    });
+    return { title, description, keywords, links, images, text }
+  })
 
-    await browser.close();
-    return pageInfo;
+  await browser.close()
+  return pageInfo
 }
 
 app.get('/api/data', async (req, res) => {
-    try {
-        const url: string = req.query.url as string;
-        if (!url) {
-            throw new Error('No se proporcionó ninguna URL');
-        }
-        const pageInfo = await getPageInfo(url);
-        await Promise.all([
-            fs.writeFile('page_info.json', JSON.stringify(pageInfo, null, 2)),
-            fs.writeFile('links.txt', pageInfo.links.join('\n')),
-            fs.writeFile('images.txt', pageInfo.images.join('\n'))
-        ]);
-        const {title, description, keywords, links, images, text} = pageInfo;
-        res.json({title, description, keywords, links, images, text});
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Error al obtener datos' });
+  try {
+    const url: string = req.query.url as string
+    if (!url) {
+      throw new Error('No se proporcionó ninguna URL')
     }
-});
+    const pageInfo = await getPageInfo(url)
+    await Promise.all([
+      fs.writeFile('page_info.json', JSON.stringify(pageInfo, null, 2)),
+      fs.writeFile('links.txt', pageInfo.links.join('\n')),
+      fs.writeFile('images.txt', pageInfo.images.join('\n'))
+    ])
+    const { title, description, keywords, links, images, text } = pageInfo
+    res.json({ title, description, keywords, links, images, text })
+  } catch (error) {
+    console.error('Error:', error)
+    res.status(500).json({ error: 'Error al obtener datos' })
+  }
+})
 
 app.listen(PORT, () => {
-    console.log(`Servidor en ejecución en el puerto ${PORT}`);
-});
+  console.log(`Servidor en ejecución en el puerto ${PORT}`)
+})
